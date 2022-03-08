597cafb1ecb1e9073045da51b2325a5df8936bb7
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2022-03-08 13:43:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function bisiesto(year) {
+	if(year % 400 == 0){
+		return true;
+	}
+	else if (year % 100 == 0) {
+		return false;
+	} else if (year % 4 == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+export default bisiesto;
